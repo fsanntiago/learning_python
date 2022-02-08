b504b665438a52cae33398f7ecd809e7f206e09f
Fabricio Santiago <zerofabricio@hotmail.com>
2022-02-08 10:51:11 -0300

    Learning Python - If

---
+++ 4-if_statements/if_Statements/exercieses/alien_colors #3.py
@@ -0,0 +1,31 @@
+# 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into
+# an if-elifelse chain.
+# •	 If the alien is green, print a message that the player earned 5
+# points.
+
+# •	 If the alien is yellow, print a message that the player earned 10
+# points.
+
+# •	 If the alien is red, print a message that the player earned 15
+# points.
+
+# Color 'green'
+alien_color = 'green'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
+
+
+# Color 'red'
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+elif alien_color == 'yellow':
+    print("You just earned 10 points!")
+else:
+    print("You just earned 15 points!")
